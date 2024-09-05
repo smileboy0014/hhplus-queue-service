7c7fc5009cf1b9c35326b428c5b03102f00898ca
gt.park <smileboy0014@gmail.com>
2024-09-05 16:34:41 +0900

    first commit

---
+++ src/main/java/com/hhplus/hhplusqueueservice/HhplusQueueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplusqueueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusQueueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusQueueServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hhplus/hhplusqueueservice/HhplusQueueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplusqueueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusQueueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
